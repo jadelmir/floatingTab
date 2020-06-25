3e7985bdec9c129420508d41097b11374d561baf
jad el mir <jadelmir>
2020-06-25 16:45:08 -0400

    Initial Commit

---
+++ floating Tab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  floating Tab
+//
+//  Created by Jad El mir on 6/25/20.
+//  Copyright © 2020 Jad El mir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
